da3d23dfd3669834544ff59599764cea91421902
antoinemansour7 <129776972+antoinemansour7@users.noreply.github.com>
2025-04-07 19:07:02 -0400

    backend and frontend connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // Fetch from the backend API
+    fetch('/api/hello')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LexiGO! Vocabulary Builder</h1>
+      <p>{message ? message : 'Loading...'}</p>
     </div>
   );
 }
